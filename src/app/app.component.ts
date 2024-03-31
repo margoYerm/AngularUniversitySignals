0570016fc313bf6c25bc72492fb1f19d90c26851
Margo Yermachenko <margoermachenko@gmail.com>
2024-03-31 23:51:28 +0200

    Signals with objects and arrays. Update data in this signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,17 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  counter = signal(0)
+  counter = signal(0);
+
+  course = signal({
+    id: 1,
+    title: "Angular For Beginners"
+  });
+
+  courses = signal([
+    "Angular For Beginners",
+    "Reactive Angular Course"
+  ])
 
   constructor() {
     const readOnlySignal = this.counter.asReadonly();
@@ -18,6 +28,13 @@ export class AppComponent {
   increment() {
     //this.counter++;
     //this.counter.set(this.counter() + 1)
-    this.counter.update( val => val + 1 )
+    this.counter.update( val => val + 1 );
+    //this.course().title = "Hello world!"; //wrong way
+    this.course.set({
+      id: 1,
+      title: "Hello world!",
+    })
+    //this.courses().push("Angular Core Deep Dive");//wrong way
+    this.courses.update(courses => [...courses, "Angular Core Deep Dive"])
   }
  } 

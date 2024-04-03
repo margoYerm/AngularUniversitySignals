360920adc78764eff98e810dcba537e77c85ff52
Margo Yermachenko <margoermachenko@gmail.com>
2024-04-03 23:55:18 +0200

    Added service with signal data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, EffectRef, computed, effect, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CounterService } from './services/counter.service';
 
 @Component({
   selector: 'app-root',
@@ -8,58 +9,21 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  counter = signal(0);
-  multiplier: number = 0;
-
-  course = signal({
-    id: 1,
-    title: "Angular For Beginners"
-  });
-
-  courses = signal([
-    "Angular For Beginners",
-    "Reactive Angular Course"
-  ])
+export class AppComponent {   
 
   derivedCounter = computed(
     () => { 
-      const counter = this.counter();             
+      const counter = this.counterService.counter();             
       return counter * 10; 
     }
-  )
+  ) 
 
-  effectRef: EffectRef;
-
-  constructor() {    
-    this.effectRef = effect((onCleanup) => {
-      onCleanup(() => {
-        console.log(`Cleanup occurred!`);
-      })
-      const counterValue = this.counter();
-      const derivedCounterValue = this.derivedCounter();      
-      console.log(`counter: ${counterValue}; derived counter: ${derivedCounterValue}`);
-    }, {
-      manualCleanup: true
-    })
+  constructor(public counterService: CounterService) {
+    console.log(counterService)
   }
 
-  increment() {    
-    //this.counter.set(this.counter() + 1)
-    this.counter.update( val => val + 1 );    
-    /*this.course.set({
-      id: 1,
-      title: "Hello world!",
-    })*/    
-    //this.courses.update(courses => [...courses, "Angular Core Deep Dive"])
-  }
-
-  incrementMultiplier() {
-    this.multiplier++;
-  }
-
-  onCleanup() {
-    this.effectRef.destroy()
+  increment() {        
+    this.counterService.increment();     
   }
   
  } 
